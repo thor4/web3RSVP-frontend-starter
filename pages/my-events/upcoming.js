5a918f364373647b1aa9479007382dede3a1d081
thor4 <6412103+thor4@users.noreply.github.com>
2022-08-20 18:47:50 -0400

    create past rsvps and start upcoming events

--- pages/my-events/upcoming.js
+++ pages/my-events/upcoming.js
@@ -1,4 +1,25 @@
 import Dashboard from "../../components/Dashboard";
+import { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+import EventCard from "../../components/EventCard";
+
+const MY_UPCOMING_EVENTS = gql`
+  query Events($eventOwner: String, $currentTimestamp: String) {
+    events(
+      where: { eventOwner: $eventOwner, eventTimestamp_gt: $currentTimestamp }
+    ) {
+      id
+      eventID
+      name
+      description
+      eventTimestamp
+      maxCapacity
+      totalRSVPs
+      imageURL
+    }
+  }`;
 
 export default function MyUpcomingEvents() {
   
